02956d2ef079c8a56c189a0b5c46b7db814f9ea7
Petro Pochynok <p.pochynok123@gmail.com>
2023-03-02 13:34:49 +0200

    create User and UserProfile entities with mapping

---
+++ src/main/java/com/example/gamingservice/model/User.java
@@ -0,0 +1,41 @@
+package com.example.gamingservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @Column(nullable = false)
+    private String gender;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToOne(mappedBy = "user")
+    private UserProfile userProfile;
+
+}
---
+++ src/main/java/com/example/gamingservice/model/UserProfile.java
@@ -0,0 +1,33 @@
+package com.example.gamingservice.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_profile")
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String street;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+}
