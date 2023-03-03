0d6b26b4e3cfd3b6f453a1f1da274c547d7d5ce2
Petro Pochynok <p.pochynok123@gmail.com>
2023-03-03 13:59:47 +0200

    create 1 exception instead of 2

--- src/main/java/com/gamingservice/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.gamingservice.exception;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-
-}
